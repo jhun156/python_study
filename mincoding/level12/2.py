c20d29ef07ff271ae341418f2298fe9deb3b7d17
jihun <qkrwlgns156@naver.com>
2025-01-28 16:41:37 +0900

    12번 풀이중

---
+++ mincoding/level12/2.py
@@ -0,0 +1,6 @@
+a = input()
+b = input()
+lst = [a,b]
+
+for i in lst:
+    print(len(i),end = ' ')
---
+++ mincoding/level12/3.py
@@ -0,0 +1,7 @@
+a = int(input())
+
+for i in range(4):
+    for j in range(4):
+        print(a-i,end='')
+    print()
+
