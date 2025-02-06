02cf5900d852f2a1fe13afa79312116f9f892ffa
jihun <qkrwlgns156@naver.com>
2025-02-06 20:29:03 +0900

    1

--- SWEA/0205/1206.View.py
+++ SWEA/0205/1206.View.py
@@ -1,6 +1,27 @@
 import sys
 sys.stdin = open("input.txt","r")
 
-T = int(input())
+T = 10
 for test_case in range(1, T + 1):
-    pass
+    num = int(input())
+    input_list = [i for i in map(int,input().split())]
+    main_list = [0,0,*input_list,0,0]
+    def get_point(index):
+        temp_list = [main_list[index + i] for i in range(5)]
+        temp_max = max(temp_list)
+        result = 0
+        result_list = []
+        for i in range(5):
+            result_list.append(temp_list[i] - temp_max)
+        for i in range(5):
+            if result_list[i] > 0:
+                result += result_list[i]
+        return result
+
+    answer = 0
+    for i in range(10):
+        temp = 0
+        temp += get_point(i)
+        answer += temp
+
+    print(f"#{test_case} {answer}")
