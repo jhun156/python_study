b3588856a053b24c2758aef3ea1ca19a921d0082
박지훈 <qkrwlgns156@naver.com>
2025-02-07 11:52:21 +0900

    1

--- SWEA/0206/13565.electric_bus.py
+++ SWEA/0206/13565.electric_bus.py
@@ -1,5 +1,26 @@
 import sys
 sys.stdin = open("input.txt","r")
 
-T = int(input())
-for test_case in range(1,T+1):
+t = int(input())
+
+for case_num in range(1, t + 1):
+    k, n, m = map(int, input().split())
+    stations = list(map(int, input().split()))
+
+    location = 0
+    cnt = 0
+
+    while location + k < n:
+        next_location = location + k
+
+        while next_location > location and next_location not in stations:
+            next_location -= 1
+
+        if next_location == location:
+            cnt = 0
+            break
+
+        location = next_location
+        cnt += 1
+
+    print(f"#{case_num} {cnt}")
