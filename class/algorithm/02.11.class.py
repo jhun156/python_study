1fdd91353ab0039e24457de0586b5efe1f7ec993
박지훈 <qkrwlgns156@naver.com>
2025-02-12 08:47:09 +0900

    1

--- class/algorithm/02.11.class.py
+++ class/algorithm/02.11.class.py
@@ -1,13 +1,13 @@
 def abc(level):
-    print("1")
     global cnt
 
-    if level==2:
+    if level==3:
         return
 
-    for i in range(2):
+    for i in range(3):
+        cnt += 1
         abc(level+1)
-
+        cnt += 1
 cnt = 0
 abc(0)
 print(cnt)
