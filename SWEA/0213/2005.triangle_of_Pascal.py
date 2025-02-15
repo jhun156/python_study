67256ff73a2a047e51a9c3a5c9f784af9130c669
jihun <qkrwlgns156@naver.com>
2025-02-15 12:59:35 +0900

    1

--- SWEA/0213/2005.triangle_of_Pascal.py
+++ SWEA/0213/2005.triangle_of_Pascal.py
@@ -1,16 +1,19 @@
-import sys
-sys.stdin = open("input.txt","r")
+# import sys
+# sys.stdin = open("input.txt","r")
 
-t = int(input())
-for test in range(1, t + 1):
-    n = int(input())
-    lst = [[] for i in range(n)]
-    for i in range(n):
-        for j in range(i + 1):
+def triangle(num):
+    lst = [[] for _ in range(num)]
+    for i in range(num):
+        for j in range(i+1):
             if j == 0 or j == i:
                 lst[i].append(1)
             else:
-                lst[i].append(lst[i - 1][j - 1] + lst[i - 1][j])
-    print(f'#{test}')
-    for i in lst:
-        print(*i)
+                lst[i].append(lst[i-1][j-1]+lst[i-1][j])
+    return lst
+
+T = int(input())
+for tc in range(1,T+1):
+    ans = triangle(tc)
+    print(f"#{tc}")
+    for i in range(tc):
+        print(*ans[i])
