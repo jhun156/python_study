50d111b7d5f43fd97999cd4b6aec0ad6e13634f9
Parkjihun <qkrwlgns156@naver.com>
2025-02-19 17:38:24 +0900

    0219 class

--- class/algorithm/02.19.class.py
+++ class/algorithm/02.19.class.py
@@ -1,3 +1,4 @@
+'''
 # stack (Last In First Out, LIFO)
 st=list()
 st.append(2)
@@ -44,7 +45,7 @@ q.put(25)   # 추가할 땐 put
 q.get()     # 제거할 땐 get
 print(q)
 print(q.queue)
-
+'''
 # DFS
 
 # 트리구조
@@ -63,25 +64,211 @@ print(q.queue)
 # 2. 인접 리스트
 # [[1,4]
 #  [2,3]] 방식
-
-name = 'ABCDEF'
-arr = [
+'''
+name="ABCDEF"
+arr=[
     [0,1,1,0,0,0],
     [0,0,0,1,1,0],
     [0,0,0,0,0,1],
     [0,0,0,0,0,0],
     [0,0,0,0,0,0],
-    [0,0,0,0,0,0],
-]
+    [0,0,0,0,0,0]]
 
 def dfs(now):
     print(name[now],end=' ')
     for x in range(6):
-        if arr[now][x] == 1:
+        if arr[now][x]==1:
             dfs(x)
 
-dfs(0)
+dfs(0) # 트리 탐색을 할 인덱스 적어주기
+'''
 
+'''
+# tree를 인접리스트로 표현 후 DFS 탐색하기
+import sys
+sys.stdin = open("input.txt","r")
+
+name="ABCDEF"
+N,M = map(int,input().split())
+arr = [[] for _ in range(N)]
+for i in range(M):
+    si,do = map(int,input().split())
+    arr[si].append(do)
+
+def dfs(now):
+
+    print(now, end=' ')
+    for i in arr[now]:
+        dfs(i)
+
+dfs(0)
+'''
+
+'''
+# Tree 구조가 아닐때 visited를 활용하는 법
+name = 'ACBD'
+arr =[
+    [0,1,1,0],
+    [1,0,0,1],
+    [0,1,0,1],
+    [0,0,0,0],
+]
+
+visited = [0] * 4
+visited[0] = 1
+
+def dfs(now):
+
+    print(name[now],end='')
+    for i in range(4):
+        if arr[now][i] == 1 and visited[i] == 0:
+            visited[i] = 1
+            dfs(i)
+
+dfs(0)
+'''
+
+'''
+# 경로 갯수 검색
+name = 'ABCD'
+arr = [
+    [0,1,1,0],
+    [0,0,1,1],
+    [0,1,0,1],
+    [0,0,0,0],
+]
+visited = [0] * 4
+visited[0] = 1
+cnt = 0
+
+def dfs(now):
+
+    global cnt
+    if now == 3:
+        cnt += 1
+    for i in range(4):
+        if visited[i] == 0 and arr[now][i] == 1:
+            visited[i] = 1
+            dfs(i)
+            visited[i] = 0
+
+dfs(0)
+print(cnt)
+'''
+
+'''
+# 가중치가 있는 경로 탐색
+arr = [
+    [0,1,8,0],
+    [0,0,1,7],
+    [0,1,0,1],
+    [0,0,0,0],
+]
+visited = [0] * 4
+visited[0] = 1
+Min = 21e8
+
+def dfs(now,Sum):
+
+    global Min
+    if now == 3:
+        Min=min(Min,Sum)
+
+    for i in range(4):
+        if visited[i] == 0 and arr[now][i] != 0:
+            visited[i] = 1
+            dfs(i,Sum+arr[now][i])
+            visited[i] = 0
+
+dfs(0,0)
+print(Min)
+'''
+
+'''
+# 미로 찾기
+
+maze = [
+    [0,0,0,0,0],
+    [0,0,1,1,0],
+    [1,1,0,1,0],
+    [0,1,0,1,0],
+    [0,1,0,1,0],
+]
+visited = [[0] * 5 for _ in range(5)]
+flag = 0
+directY = [0,0,1,-1]
+directX = [1,-1,0,0]
+
+def dfs(nowy,nowx):
+
+    global flag
+    if nowy == 3 and nowx == 2:
+        flag = 1
+        return
+
+    for i in range(4):
+        dy = nowy + directY[i]
+        dx = nowx + directX[i]
+        if dy < 0 or dy > 4 or dx < 0 or dx > 4:
+            continue
+        if maze[dy][dx] == 1:
+            continue
+        if visited[dy][dx] == 1:
+            continue
+        visited[dy][dx] = 1
+        dfs(dy,dx)
+        if flag:
+            return
+
+visited[0][0] = 1
+dfs(0,0)
+if flag:
+    print("가능")
+else:
+    print("불가능")
+'''
+
+'''
+# 한 지점에서 다른 지점으로 최소 이동거리 구하기
+arr = [
+    [0,0,0,0,1],
+    [0,0,1,0,1],
+    [1,0,1,0,1],
+    [1,0,1,0,0],
+    [1,0,0,0,0],
+]
+
+visited = [[0] * 5 for _ in range(5)]
+Min = 21e8
+directY = [0,0,1,-1]
+directX = [1,-1,0,0]
+cnt = 0
+
+def dfs(nowy,nowx,level):
+
+    global Min,cnt
+    if nowy == 4 and nowx == 2:
+        Min = min(level,Min)
+        cnt = 0
+        return
+
+    for i in range(4):
+        dy = nowy + directY[i]
+        dx = nowx + directX[i]
+        if dy < 0 or dy > 4 or dx < 0 or dx > 4:
+            continue
+        if arr[dy][dx] == 1:
+            continue
+        if visited[dy][dx] == 1:
+            continue
+        visited[dy][dx] = 1
+        dfs(dy,dx,level+1)
+        visited[dy][dx] = 0
+
+visited[0][0] = 1
+dfs(0,0,0)
+print(Min)
+'''
 
 
 
