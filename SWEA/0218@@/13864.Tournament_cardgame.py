28d39324186edfa22fb3a35651f253aa3feff528
Parkjihun <qkrwlgns156@naver.com>
2025-02-21 17:43:55 +0900

    1

--- SWEA/0218@@/13864.Tournament_cardgame.py
+++ SWEA/0218@@/13864.Tournament_cardgame.py
@@ -3,35 +3,29 @@ sys.stdin = open("input.txt","r")
 
 def GBB(a,b):
     if arr[a] == arr[b]:
-        return a
+        return min(a,b)
     elif abs(arr[a]-arr[b]) == 1:
-        if arr[a] > arr[b]:
-            return a
-        else:
-            return b
+        return a if arr[a] > arr[b] else b
     elif abs(arr[a]-arr[b]) == 2:
-        if arr[a] > arr[b]:
-            return b
-        else:
-            return a
+        return a if arr[a] < arr[b] else b
 
 def divide(arr):
     n = len(arr)
     if n == 1:
-        return arr
+        return arr[0]
     mid = (n-1)//2 + 1
     front = arr[:mid]
     back = arr[mid:]
 
-    front = divide(front)
-    back = divide(back)
-    return GBB(front[0],back[0])
+    front_winner = divide(front)
+    back_winner = divide(back)
+    return GBB(front_winner,back_winner)
 
 T = int(input())
 for tc in range(1,T+1):
     N = int(input())
     arr = list(map(int,input().split()))
     arr.insert(0,0)  # 0번 인덱스에 0을 추가 인덱스 번호를 맞추기 위함
-    ans = divide(list(range(1,N)))
-    print(f"#{tc} {ans}")
+    ans = divide(list(range(1,N+1)))
+    print(f"#{tc} {arr[ans]}")
     # 최종으로 이긴 사람의 인덱스 + 1 이 답
