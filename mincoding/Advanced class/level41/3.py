ca3eda06fd30777449145cc751b1a924edb49499
Parkjihun <qkrwlgns156@naver.com>
2025-03-14 17:48:34 +0900

    1

--- mincoding/Advanced class/level41/3.py
+++ mincoding/Advanced class/level41/3.py
@@ -1,4 +1,20 @@
-import sys
-sys.stdin = open("input.txt","r")
+# import sys
+# sys.stdin = open("input.txt","r")
 
-T, n = map(int,input().split())
+T, N = map(int,input().split())
+coins = list(map(int,input().split()))
+DP = [21e8] * (T+1)
+DP[0] = 0
+
+def find(n):
+
+    for coin in coins:
+        for i in range(coin,T+1):
+            DP[i] = min(DP[i], DP[i-coin] + 1)
+
+    if DP[n] != 21e8:
+        return DP[n]
+    else:
+        return "impossible"
+
+print(find(T))
