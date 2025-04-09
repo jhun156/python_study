a67ee66d505d3e536fc2bb06b642ebcffc4f8057
parkjihun <qkrwlgns156@naver.com>
2025-04-09 17:52:24 +0900

    1

---
+++ django_review/movies/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Comment, Movie
+
+class MovieForm(forms.ModelForm):
+    class Meta:
+        model = Movie
+        fields = ('title', 'description',)
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        exclude = ('movie', 'user')
