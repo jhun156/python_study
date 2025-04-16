503b037fdddf18f1207af864eb4b9f342df665d7
하기환 <hgh9940@naver.com>
2025-04-16 10:10:57 +0900

    1

---
+++ django/django_package_list.py
@@ -0,0 +1,7 @@
+'''
+pip install django
+pip install djangorestframework # settings.py에 rest_framework 추가
+pip install requests
+pip install ipython
+pip install django-extensions # settings.py에 django_extensions 추가
+'''
